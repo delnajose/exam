5a95d60c54e966d2c134a681a34b02df72be7da6
delna <delna@gmail.com>
2018-04-05 14:19:39 +0530

    file1 committed

---
+++ second.sh
@@ -0,0 +1,33 @@
+x=1
+res=0
+while [ $x -eq 1 ]
+do
+echo "enter two numbers"
+read a
+read b
+echo "options"
+echo "1.addition"
+echo "2.subtraction"
+echo "3.multiplication"
+echo "4.division"
+echo "choose any option"
+read n
+case $n in
+1) res=` expr $a + $b `
+echo "resul = "$res;;
+2) res=` expr $a - $b `
+echo "resul = "$res;;
+3) res=` expr $a \* $b `
+echo "resul = "$res;;
+4) res=` expr $a / $b `
+echo "resul = "$res;;
+*)echo "invalid option";;
+esac
+echo "do you want to  continue press 1"
+read t
+if [ $t -ne 1 ]
+then
+exit
+fi
+done
+
